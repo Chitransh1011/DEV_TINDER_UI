c2157435adbebb9ec464191fa7aa76054e1b84e9
CHITRANSH PRASANNA <145662598+Chitransh1011@users.noreply.github.com>
2025-07-19 10:55:37 +0530

    Added Login Page

--- devtinder-web/src/App.jsx
+++ devtinder-web/src/App.jsx
@@ -1,23 +1,19 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Body from "./Body";
 import Login from "./Login";
+import Profile from "./Profile";
+import Connections from "./Connections";
 function App() {
   return (
     <BrowserRouter>
-    
-    <Routes>
-      <Route path="/" element={<Body/>}>
-        <Route path="/login" element={<Login/>}/>      
-
-      
-      </Route>
-
-
-    </Routes>
-    
-    
+      <Routes>
+        <Route path="/" element={<Body />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/connections" element={<Connections />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
-    
   );
 }
 
--- devtinder-web/src/Login.jsx
+++ devtinder-web/src/Login.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import axios from "axios";
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [emailId, setEmailId] = useState("");
+  const [password, setPassword] = useState("");
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post(
+        "http://localhost:3000/login",
+        {
+          emailId,
+          password,
+        },
+        { withCredentials: true }
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-export default Login
+  return (
+    <div className="flex justify-center mt-8">
+      <div className="card card-border bg-base-300 w-96">
+        <div className="card-body">
+          <h2 className="card-title justify-center">Login</h2>
+          <div>
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend">Email</legend>
+              <input
+                type="text"
+                className="input"
+                value={emailId}
+                onChange={(e) => setEmailId(e.target.value)}
+              />
+            </fieldset>
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend">Password</legend>
+              <input
+                type="text"
+                className="input"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </fieldset>
+          </div>
+          <div className="card-action justify-center pt-2">
+            <button className="btn btn-primary" onClick={handleLogin}>
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
